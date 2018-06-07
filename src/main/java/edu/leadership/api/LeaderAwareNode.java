ca0b37c96c6671322eb9712367761bef69b24aa2
sergey-melnychuk <serg.meln@gmail.com>
2018-06-07 23:16:10 +0200

    Add leader-aware node implementation skeleton

--- src/main/java/edu/leadership/api/LeaderAwareNode.java
+++ src/main/java/edu/leadership/api/LeaderAwareNode.java
@@ -7,6 +7,12 @@ import java.util.Optional;
 public interface LeaderAwareNode {
 
     /**
+     * Get the next value from cluster-wide ordered leader-aware sequence
+     * @return Optional.of(number) if request succeeded, Optional.empty() otherwise
+     */
+    Optional<Long> getValue();
+
+    /**
      * Get address of current leader
      * @return Optional.of(leader address) if leader is accepted or Optional.empty() if no master is elected
      */
---
+++ src/main/java/edu/leadership/impl/LeaderVotingNode.java
@@ -0,0 +1,35 @@
+package edu.leadership.impl;
+
+import edu.common.api.Address;
+import edu.leadership.api.LeaderAwareNode;
+import edu.membership.api.Membership;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class LeaderVotingNode implements LeaderAwareNode {
+
+    private final Membership membership;
+    private final Supplier<Long> clock;
+
+    public LeaderVotingNode(Membership membership, Supplier<Long> clock) {
+        this.membership = membership;
+        this.clock = clock;
+    }
+
+    @Override
+    public Optional<Long> getValue() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Address> getLeader() {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean isLeader() {
+        return false;
+    }
+
+}
